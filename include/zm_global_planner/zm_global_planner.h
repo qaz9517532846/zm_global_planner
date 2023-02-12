092c3b44f722cb84b8fcc8d08099af9ac8fe232b
Zhe-Ming, Zhang <48936051+qaz9517532846@users.noreply.github.com>
2023-02-12 13:26:23 +0800

    Add files via upload

---
+++ include/zm_global_planner/zm_global_planner.h
@@ -0,0 +1,32 @@
+#ifndef ZM_GLOBAL_PLANNER_CPP
+#define ZM_GLOBAL_PLANNER_CPP
+
+#include <ros/ros.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <costmap_2d/costmap_2d.h>
+#include <nav_core/base_global_planner.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <angles/angles.h>
+#include <base_local_planner/world_model.h>
+#include <base_local_planner/costmap_model.h>
+#include <tf/transform_broadcaster.h>
+#include <dynamic_reconfigure/server.h>
+#include <zm_global_planner/ZMGlobalPlannerConfig.h>
+
+using namespace std;
+using std::string;
+
+namespace zm_global_planner
+{
+    class ZMGlobalPlanner : public nav_core::BaseGlobalPlanner
+    {
+        public:
+          ZMGlobalPlanner();
+          ~ZMGlobalPlanner(); 
+          ZMGlobalPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+          void initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+          bool makePlan(const geometry_msgs::PoseStamped& start, const geometry_msgs::PoseStamped& goal, std::vector<geometry_msgs::PoseStamped>& plan);
+    };
+};
+
+#endif 
